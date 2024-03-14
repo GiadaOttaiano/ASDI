ae1e5c9138578bc48b9ea9f927124fcaaccf192d
GiadaOttaiano <ottgiada@gmail.com>
2024-03-14 19:00:03 +0100

    Update shift_register_structural.vhd

--- Reti sequenziali elementari/Esercizio 4.1/Structural/shift_register_structural.vhd
+++ Reti sequenziali elementari/Esercizio 4.1/Structural/shift_register_structural.vhd
@@ -1,109 +1,105 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
-entity shift_register_structural is
-    port(
-        reg_input, rst, clk : in STD_LOGIC;
-        shift : in STD_LOGIC_VECTOR(1 downto 0);
-        reg_output : out STD_LOGIC_VECTOR(3 downto 0)
+ENTITY shift_register_structural IS
+    PORT (
+        reg_input, rst, clk : IN STD_LOGIC;
+        shift : STD_LOGIC_VECTOR(1 DOWNTO 0);
+        reg_output : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
     );
-end shift_register_structural;
+END shift_register_structural;
 
-architecture structural of shift_register_structural is
-    component mux41 is
-        port(   
-            mux41_input : in STD_LOGIC_VECTOR(3 downto 0);
-            mux41_sel : in STD_LOGIC_VECTOR(1 downto 0);
-            y0 : out STD_LOGIC
+ARCHITECTURE structural OF shift_register_structural IS
+    COMPONENT mux41 IS
+        PORT (
+            mux41_input : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+            mux41_sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+            y0 : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    COMPONENT flipflop_d IS
+        PORT (
+            D, clk, rst : IN STD_LOGIC;
+            Q : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    SIGNAL M : STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL F : STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0');
+
+BEGIN
+    mux0 : mux41
+    PORT MAP(
+        mux41_input(3) => F(2),
+        mux41_input(2) => F(1),
+        mux41_input(1) => reg_input,
+        mux41_input(0) => reg_input,
+        mux41_sel => shift,
+        y0 => M(0)
     );
-    end component;
-
-    component flip_flop_d is
-        port(
-        D, clk, rst : in STD_LOGIC;
-        Q : out STD_LOGIC
+    mux1 : mux41
+    PORT MAP(
+        mux41_input(3) => F(3),
+        mux41_input(2) => F(2),
+        mux41_input(1) => reg_input,
+        mux41_input(0) => F(0),
+        mux41_sel => shift,
+        y0 => M(1)
     );
-    end component;
+    mux2 : mux41
+    PORT MAP(
+        mux41_input(3) => reg_input,
+        mux41_input(2) => F(3),
+        mux41_input(1) => F(0),
+        mux41_input(0) => F(1),
+        mux41_sel => shift,
+        y0 => M(2)
+    );
+    mux3 : mux41
+    PORT MAP(
+        mux41_input(3) => reg_input,
+        mux41_input(2) => reg_input,
+        mux41_input(1) => F(1),
+        mux41_input(0) => F(2),
+        mux41_sel => shift,
+        y0 => M(3)
+    );
 
-    signal M : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
-    signal F : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
+    ff0 : flipflop_d
+    PORT MAP(
+        D => M(0),
+        clk => clk,
+        rst => rst,
+        Q => F(0)
+    );
+    reg_output(0) <= F(0);
 
-    begin
-        mux0 : mux41
-        port map(
-            mux41_input(3) => reg_input,
-            mux41_input(2) => reg_input,
-            mux41_input(1) => F(2),
-            mux41_input(0) => F(1),
-            mux41_sel => shift,
-            y0 => M(0) 
-        );
+    ff1 : flipflop_d
+    PORT MAP(
+        D => M(1),
+        clk => clk,
+        rst => rst,
+        Q => F(1)
+    );
+    reg_output(1) <= F(1);
 
-        mux1 : mux41
-        port map(
-            mux41_input(3) => reg_input,
-            mux41_input(2) => F(0),
-            mux41_input(1) => F(3),
-            mux41_input(0) => F(2),
-            mux41_sel => shift,
-            y0 => M(1) 
-        );
+    ff2 : flipflop_d
+    PORT MAP(
+        D => M(2),
+        clk => clk,
+        rst => rst,
+        Q => F(2)
+    );
+    reg_output(2) <= F(2);
 
-        mux2 : mux41
-        port map(
-            mux41_input(3) => F(0),
-            mux41_input(2) => F(1),
-            mux41_input(1) => reg_input,
-            mux41_input(0) => F(3),
-            mux41_sel => shift,
-            y0 => M(2) 
-        );
-
-        mux3 : mux41
-        port map(
-            mux41_input(3) => F(1),
-            mux41_input(2) => F(2),
-            mux41_input(1) => reg_input,
-            mux41_input(0) => reg_input,
-            mux41_sel => shift,
-            y0 => M(3) 
-        );
-
-        ff0 : flip_flop_d
-        port map(
-            D => M(0),
-            clk => clk,
-            rst => rst,
-            Q => F(0)
-        );
-        reg_output(0) <= F(0);
-
-        ff1 : flip_flop_d
-        port map(
-            D => M(1),
-            clk => clk,
-            rst => rst,
-            Q => F(1)
-        );
-        reg_output(1) <= F(1);
-
-        ff2 : flip_flop_d
-        port map(
-            D => M(2),
-            clk => clk,
-            rst => rst,
-            Q => F(2)
-        );
-        reg_output(2) <= F(2);
-        
-        ff3 : flip_flop_d
-        port map(
-            D => M(3),
-            clk => clk,
-            rst => rst,
-            Q => F(3)
-        );
-        reg_output(3) <= F(3);
-
-end structural;
+    ff3 : flipflop_d
+    PORT MAP(
+        D => M(3),
+        clk => clk,
+        rst => rst,
+        Q => F(3)
+    );
+    reg_output(3) <= F(3);
+END structural;
