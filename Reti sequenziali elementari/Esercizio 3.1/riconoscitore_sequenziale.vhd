684efe979f54e5b881afccd64e28d7da89b040df
GiadaOttaiano <ottgiada@gmail.com>
2024-03-14 12:02:00 +0100

    Upload

---
+++ Reti sequenziali elementari/Esercizio 3.1/riconoscitore_sequenziale.vhd
@@ -0,0 +1,90 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity riconoscitore_sequenza is
+    port(
+        i : IN STD_LOGIC;
+        m : IN STD_LOGIC;
+        a : IN STD_LOGIC;
+        y : OUT STD_LOGIC
+    );
+end riconoscitore_sequenza;
+
+architecture behavioral of riconoscitore_sequenza is
+    type stato is (q0, q1, q2, q3, q4);
+    signal stato_corrente : stato := q0;
+    signal stato_prossimo : stato;
+    signal uscita_corrente : STD_LOGIC := 'U';
+
+    begin
+        transizione : process(stato_corrente, i, m)
+        begin
+            case m is
+                when '0' =>
+                    case stato_corrente is
+                        when q0 =>
+                            if (i = '1') then
+                                stato_prossimo <= q1;
+                                uscita_corrente <= '0';
+                            else
+                                stato_prossimo <= q2;
+                                uscita_corrente <= '0';
+                            end if;
+                        when q1 =>
+                            if (i = '1') then
+                                stato_prossimo <= q3;
+                                uscita_corrente <= '0';
+                            else
+                                stato_prossimo <= q4;
+                                uscita_corrente <= '0';
+                            end if;
+                        when q2 =>
+                            stato_prossimo <= q3;
+                            uscita_corrente <= '0';
+                        when q3 =>
+                            stato_prossimo <= q0;
+                            uscita_corrente <= '0';
+                        when q4 =>
+                            stato_prossimo <= q0;
+                            if (i = '1') then
+                                uscita_corrente <= '1';
+                            else
+                                uscita_corrente <= '0';
+                            end if;
+                    end case;
+                when '1' =>
+                    case stato_corrente is
+                        when q0 => 
+                            if (i = '1') then
+                                stato_prossimo <= q1;
+                                uscita_corrente <= '0';
+                            else
+                                stato_prossimo <= q0;
+                                uscita_corrente <= '0';
+                            end if;
+                        when q1 => 
+                            if (i = '1') then
+                                stato_prossimo <= q1;
+                                uscita_corrente <= '0';
+                            else
+                                stato_prossimo <= q2;
+                                uscita_corrente <= '0';
+                            end if;
+                        when q2 => 
+                            stato_prossimo <= q0;
+                            if (i = '1') then
+                                uscita_corrente <= '1';
+                            else
+                                uscita_corrente <= '0';
+                            end if;
+                    end case;
+        end process;
+
+        tempificazione : process(a)
+        begin
+            if (a'event and a = '1') then
+                stato_corrente <= stato_prossimo;
+                y <= uscita_corrente;
+            end if;
+        end process;
+end behavioral;
---
+++ Reti sequenziali elementari/Esercizio 3.1/riconoscitore_sequenziale_tb.vhd
@@ -0,0 +1,90 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+ENTITY riconoscitore_sequenziale_tb IS
+END riconoscitore_sequenziale_tb;
+
+ARCHITECTURE behavioral OF riconoscitore_sequenziale_tb IS
+
+    SIGNAL input : STD_LOGIC := 'U';
+    SIGNAL mode : STD_LOGIC := 'U';
+    SIGNAL temp : STD_LOGIC;
+    SIGNAL output : STD_LOGIC := 'U';
+
+    COMPONENT riconoscitore_sequenziale IS
+        port(
+            i : IN STD_LOGIC;
+            m : IN STD_LOGIC;
+            a : IN STD_LOGIC;
+            y : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+BEGIN
+    uut : riconoscitore_sequenziale
+    PORT MAP(
+        i <= input,
+        m <= mode,
+        a <= temp,
+        y <= output
+    );
+
+    temp_process : PROCESS
+    BEGIN
+        temp <= '0';
+        WAIT FOR 5 ns;
+        temp <= '1';
+        WAIT FOR 5 ns;
+    END PROCESS;
+
+    stim_proc : PROCESS
+    BEGIN
+        i <= '0';
+        m <= '0';
+        WAIT FOR 10 ns;
+
+        i <= '1';
+        m <= '0';
+        WAIT FOR 10 ns;
+
+        i <= '1';
+        m <= '0';
+        WAIT FOR 10 ns;
+
+        i <= '0';
+        m <= '0';
+        WAIT FOR 10 ns;
+
+        i <= '1';
+        m <= '0';
+        WAIT FOR 10 ns;
+
+        i <= '0';
+        m <= '1';
+        WAIT FOR 10 ns;
+
+        i <= '1';
+        m <= '1';
+        WAIT FOR 10 ns;
+
+        i <= '0';
+        m <= '1';
+        WAIT FOR 10 ns;
+
+        i <= '1';
+        m <= '1';
+        WAIT FOR 10 ns;
+
+        i <= '0';
+        m <= '1';
+        WAIT FOR 10 ns;
+
+        i <= '0';
+        m <= '0';
+        WAIT FOR 10 ns;
+
+        WAIT;
+    END PROCESS;
+
+END PROCESS;
