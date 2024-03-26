2f8afcc3ccc766b939853b30a3693179ec0221e5
GiadaOttaiano <ottgiada@gmail.com>
2024-03-26 13:56:31 +0100

    handshaking

--- Comunicazione con handshaking/Esercizio 8.1/nodeA.vhd
+++ Comunicazione con handshaking/Esercizio 8.1/nodeA.vhd
@@ -1,3 +1,74 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+
+entity nodeA is
+    port(
+        start_A, clock_A, reset_A : IN std_logic;
+        ACK_A : IN std_logic;
+        DATA_READY_A : OUT std_logic;
+        data : OUT std_logic_vector(7 downto 0)
+    );
+end nodeA;
+
+architecture structural of nodeA is
+    component cuA is
+        port(
+            cuA_start : IN std_logic;
+            cuA_clock, cuA_reset : IN std_logic;
+
+            -- CONTATORE
+            cuA_stop_count : IN std_logic_vector(3 downto 0);
+            cuA_count_enable : OUT std_logic;
+
+            -- MEM
+            cuA_read_mem : OUT std_logic;
+
+            -- HANDSHAKE
+            ACK : IN std_logic;
+            DATA_READY : OUT std_logic
+        );
+    end component;
+
+    component OU_A is
+        generic(
+            N : positive := 16;
+            M : positive := 2**3;
+            DEPTH : positive := 4   
+        );
+        port(
+            ouA_clock : IN std_logic;
+            ouA_enable, ouA_reset : IN std_logic;
+            ouA_read : IN std_logic;
+            ouA_output : OUT std_logic_vector(M-1 downto 0);
+            ouA_count : OUT std_logic_vector(DEPTH-1 downto 0)
+        );
+    end OU_A;
+
+    signal temp_count : std_logic_vector(3 downto 0);
+    signal temp_enable : std_logic;
+    signal temp_read : std_logic;
+
+    begin
+        control_unit_A : cuA
+            port map(
+                cuA_start => start_A,
+                cuA_clock => clock_A,
+                cuA_reset => reset_A,
+                cuA_stop_count => temp_count,
+                cuA_count_enable => temp_enable,
+                cuA_read_mem => temp_read,
+                ACK => ACK_A,
+                DATA_READY => DATA_READY_A
+            );
+        
+        operative_unit_A : OU_A
+            port map(
+                ouA_clock => clock_A,
+                ouA_enable => temp_enable,
+                ouA_reset => reset_A,
+                ouA_read => temp_read,
+                ouA_output => data,
+                ouA_count => temp_count
+            );
+end structural;
---
+++ Comunicazione con handshaking/Esercizio 8.1/operative_unit_B.vhd
@@ -0,0 +1,93 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity OU_B is
+    generic(
+        N : positive := 16; -- Numero stringhe
+        M : positive := 2**3;  -- Lunghezza stringhe
+        DEPTH : positive := 4   
+    );
+    port(
+        ouB_clock : IN std_logic;
+        ouB_enable, ouB_reset : IN std_logic;
+        ouB_read, ouB_write : IN std_logic;
+        ouB_A_data : IN std_logic_vector(M-1 downto 0);
+        ouB_sum : IN std_logic;     -- Segnale di abilitazione alla somma
+        oub_count : OUT std_logic_vector(DEPTH-1 downto 0)
+    );
+end OU_A;
+
+architecture structural of OU_B is
+
+    component counter_mod_N is
+        generic(
+            N : positive := 16;
+            DEPTH : positive := 4   
+        );
+        port(
+            c_clock, c_enable, c_reset : IN std_logic;
+            c_output : OUT std_logic_vector(DEPTH - 1 downto 0)
+        );
+    end component;
+
+    component B_mem is
+        generic(
+            N : positive := 16;
+            M : positive := 2**3;
+            DEPTH : positive := 4   
+        );
+        port(
+            B_address : IN std_logic_vector(DEPTH-1 downto 0);
+            B_clock : IN std_logic;
+            B_read : IN std_logic;
+            B_output : OUT std_logic_vector(M-1 downto 0)
+        );
+    end component;
+
+    component B_register is
+        generic(
+            N : positive := 16;
+            M : positive := 2**3;
+            DEPTH : positive := 4
+        );
+        port(
+            reg_address : IN std_logic_vector(DEPTH-1 downto 0);
+            reg_data : IN std_logic_vector(M-1 downto 0);
+            reg_clock, reg_write : IN std_logic;
+            reg_out : OUT std_logic_vector(M-1 downto 0)
+        );
+    end component;
+
+    signal count_output : std_logic_vector(3 downto 0);
+    signal mem_output : std_logic_vector(7 downto 0);
+    signal temp_sum : std_logic_vector(7 downto 0) := (others => 'U');
+
+    begin
+        counter_B : counter_mod_N
+            port map(
+                c_clock => ouB_clock,
+                c_enable => ouB_enable,
+                c_reset => ouB_reset,
+                c_output => count_output
+            );
+
+        memory_B : B_mem
+            port map(
+                B_address => count_output,
+                B_clock => ouB_clock,
+                B_read => ouB_read,
+                B_output => mem_output
+            );
+        
+        reg_B : B_register
+            port map(
+                reg_address => count_output,
+                reg_data => temp_sum,
+                reg_clock => ouB_clock,
+                reg_write => ouB_write
+            );
+
+        temp_sum <= std_logic_vector(unsigned(ouB_A_data) + unsigned(mem_output)) when ouB_sum = '1';
+        oub_count <= count_output;
+end structural;
