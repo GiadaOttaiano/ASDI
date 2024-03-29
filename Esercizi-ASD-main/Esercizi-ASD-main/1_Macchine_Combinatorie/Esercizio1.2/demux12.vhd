e90d8d318d9c61146b9cbfdcabda3dafe104fe6f
GiadaOttaiano <ottgiada@gmail.com>
2024-03-29 09:36:38 +0100

    Update demux12.vhd

--- Esercizi-ASD-main/Esercizi-ASD-main/1_Macchine_Combinatorie/Esercizio1.2/demux12.vhd
+++ Esercizi-ASD-main/Esercizi-ASD-main/1_Macchine_Combinatorie/Esercizio1.2/demux12.vhd
@@ -1,15 +1,16 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
-entity demux12 is 
-    port(   demux_12_in  : in STD_LOGIC;
-            demux_12_sel   : in STD_LOGIC;
-            demux_12_out  : out STD_LOGIC_VECTOR(1 downto 0)
+ENTITY demux12 IS
+    PORT (
+        demux_12_in : IN STD_LOGIC;
+        demux_12_sel : IN STD_LOGIC;
+        demux_12_out : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
     );
-end demux12;
+END demux12;
 
-architecture dataflow of demux12 is
-begin
+ARCHITECTURE dataflow OF demux12 IS
+BEGIN
     demux_12_out(0) <= ((NOT demux_12_sel) AND demux_12_in);
     demux_12_out(1) <= (demux_12_sel AND demux_12_in);
-end dataflow;
+END dataflow;
