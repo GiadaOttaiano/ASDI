81bf6b93c22cd992163f38288dee476a5b3be1b4
GiadaOttaiano <ottgiada@gmail.com>
2024-05-12 19:58:17 +0200

    omega network

--- Switch Multistadio/Esercizio 11.1/omega_network_tb.vhd
+++ Switch Multistadio/Esercizio 11.1/omega_network_tb.vhd
@@ -37,7 +37,7 @@ begin
     begin
         -- Test 1
         sel_tb <= "0001";
-        msg0_tb <= "101";   -- priorità più alta
+        msg0_tb <= "101";       
         msg1_tb <= "010";
         msg2_tb <= "111";
         msg3_tb <= "001";
@@ -46,8 +46,8 @@ begin
         -- Test 2
         sel_tb <= "1010";
         msg0_tb <= "011";
-        msg1_tb <= "100";
-        msg2_tb <= "001";   -- priorità più alta
+        msg1_tb <= "100";     
+        msg2_tb <= "001";       
         msg3_tb <= "110";
         wait for 50 ns;
         wait;
